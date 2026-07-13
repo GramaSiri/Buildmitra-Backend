372250904fc32bb99e6c18f0572e34baf7697c2b
Reddy <info@buildmitra.com>
2026-07-13 13:26:56 +0530

    Fix beta frontend CORS origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://192.168.31.248:3000', 'https://buildmitra.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://192.168.31.248:3000', 'https://buildmitra-frontend.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
@@ -124,4 +124,5 @@ app.listen(PORT, "0.0.0.0", () => {
 
 module.exports = app;
 // Force redeploy
+
 
